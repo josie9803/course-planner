5413379689eff4d1369e96bac192caad918fa668
NathanLao <75557717+nathanlao@users.noreply.github.com>
2024-04-09 20:02:40 -0700

    api: get list of departments (#7)

--- CoursePlanner/src/main/java/ca/courseplanner/AllApiDtoClasses/ApiDepartmentDTO.java
+++ CoursePlanner/src/main/java/ca/courseplanner/AllApiDtoClasses/ApiDepartmentDTO.java
@@ -1,6 +1,15 @@
 package ca.courseplanner.AllApiDtoClasses;
 
+import ca.courseplanner.model.Department;
+
 public class ApiDepartmentDTO {
     public long deptId;
     public String name;
+
+    public static ApiDepartmentDTO makeFromDepartment(Department department, int deptId) {
+        ApiDepartmentDTO departmentDTO = new ApiDepartmentDTO();
+        departmentDTO.deptId = deptId;
+        departmentDTO.name = department.getName();
+        return departmentDTO;
+    }
 }
